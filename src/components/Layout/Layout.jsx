7057ed53d690cf58af1d8d9e8e057fdd34121741
Michael <fidd.michael@gmail.com>
2023-09-11 20:52:43 +0200

    bug fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Header } from 'components/Header/Header';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+
 import { StyledList, StyledItem } from "./SearchResults.styled"
 
 export const SearchResults = ({results}) => {
